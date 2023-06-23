971a70b0f0ab25adbe1ddf727f938a2f023993a8
NaseerHines <Hinesnaseer@gmail.com>
2023-06-23 18:30:07 -0500

    Tables created for the DB

--- db/init_db.js
+++ db/init_db.js
@@ -9,11 +9,13 @@ async function buildTables() {
     // drop tables in correct order
     console.log("Dropping All Tables...");
     await client.query(`
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS sales;
     DROP TABLE IF EXISTS reviews;
     DROP TABLE IF EXISTS products_in_cart;
-    DROP TABLE IF EXISTS product;
-    DROP TABLE IF EXISTS category;
-    DROP TABLE IF EXISTS cart;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS categories;
+    DROP TABLE IF EXISTS carts;
     DROP TABLE IF EXISTS users;
     `);
     // build tables in correct order
@@ -21,39 +23,51 @@ async function buildTables() {
     await client.query(`
     CREATE TABLE users(
       id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
       email VARCHAR(255) UNIQUE NOT NULL,
       password VARCHAR(255) NOT NULL,
+      rating INTEGER,
       isAdmin BOOLEAN DEFAULT false
     );
-    CREATE TABLE cart(
+    CREATE TABLE carts(
       id SERIAL PRIMARY KEY,
-      "userid" INTEGER REFERENCES users(id),
+      "usersid" INTEGER REFERENCES users(id),
       status VARCHAR(255) NOT NULL
     );
-    CREATE TABLE category(
+    CREATE TABLE categories(
       id SERIAL PRIMARY KEY,
       name VARCHAR(255) UNIQUE NOT NULL
     );
-    CREATE TABLE product(
+    CREATE TABLE products(
       id SERIAL PRIMARY KEY,
+      "usersid" INTEGER REFERENCES users(id),
       name VARCHAR(255) UNIQUE NOT NULL,
       description TEXT NOT NULL,
       price INTEGER,
-      "categoryid" INTEGER REFERENCES category(id)
+      "categoryid" INTEGER REFERENCES categories(id)
     );
     CREATE TABLE products_in_cart(
       id SERIAL PRIMARY KEY,
-      "productid" INTEGER REFERENCES product(id),
-      "cartid" INTEGER REFERENCES cart(id)
+      "productsid" INTEGER REFERENCES products(id),
+      "cartsid" INTEGER REFERENCES carts(id)
     );
     CREATE TABLE reviews(
       id SERIAL PRIMARY KEY,
-      "userid" INTEGER REFERENCES users(id),
-      password VARCHAR(255) NOT NULL,
-      isAdmin BOOLEAN DEFAULT false
+      "usersid" INTEGER REFERENCES users(id)
+    );
+    CREATE TABLE sales(
+      id SERIAL PRIMARY KEY,
+      "usersid" INTEGER REFERENCES users(id),
+      "productsid" INTEGER REFERENCES products(id)
+    );
+    CREATE TABLE orders(
+      id SERIAL PRIMARY KEY,
+      "usersid" INTEGER REFERENCES users(id),
+      "products_in_cartid" INTEGER REFERENCES products_in_cart(id)
     );
     `);
   } catch (error) {
+    console.log(error);
     throw error;
   }
 }
@@ -71,6 +85,6 @@ async function populateInitialData() {
 
 client.connect();
 buildTables()
-  .then(populateInitialData)
+  // .then(populateInitialData)
   .catch(console.error)
   .finally(() => client.end());
