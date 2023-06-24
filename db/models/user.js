f50ca7c1375e2af34d6a6ae94dc79ea4165f5dfd
alecanthony <alecanthony39@gmail.com>
2023-06-24 07:52:46 -0500

    Made core user models

--- db/models/user.js
+++ db/models/user.js
@@ -1,11 +1,68 @@
 // grab our db client connection to use with our adapters
-const client = require('../client');
+const client = require("../client");
 
 module.exports = {
   // add your database adapter fns here
   getAllUsers,
+  createUser,
+  getUserById,
+  getUser,
 };
 
+async function createUser({ email, username, password, isAdmin }) {
+  const {
+    rows: [user],
+  } = await client.query(
+    `INSERT INTO users(email, username, password, isAdmin)
+      VALUES($1, $2, $3, $4)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING *
+      `,
+    [email, username, password, isAdmin]
+  );
+  delete user.password;
+  return user;
+}
+
 async function getAllUsers() {
-  /* this adapter should fetch a list of users from your db */
+  const { rows } = await client.query(
+    `
+  SELECT * 
+  FROM users
+`
+  );
+  return rows;
+}
+
+async function getUser({ email, password }) {
+  const { rows: user } = await client.query(
+    `
+  SELECT * 
+  FROM users
+  WHERE email=$1`,
+    [email]
+  );
+
+  if (user[0].password === password) {
+    delete user[0].password;
+    return user[0];
+  }
+}
+
+async function getUserById(userId) {
+  try {
+    const { rows: user } = await client.query(
+      `SELECT * 
+    FROM users
+    WHERE id=${userId}`
+    );
+    if (!user) {
+      return null;
+    }
+    delete user[0].password;
+
+    return user[0];
+  } catch (error) {
+    throw error;
+  }
 }
