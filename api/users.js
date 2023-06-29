43a61b1ae841a047c4c9869ef0b2815705ae6bd5
alecanthony <alecanthony39@gmail.com>
2023-06-28 19:35:40 -0500

    login and register api

---
+++ api/users.js
@@ -0,0 +1,103 @@
+require("dotenv").config();
+/* eslint-disable no-useless-catch */
+const express = require("express");
+const { createUser, getUser, getUserByUsername } = require("../db/models/user");
+const jwt = require("jsonwebtoken");
+
+const { requireUser } = require("./utils");
+const { getUserByEmail } = require("../db/models/user");
+const router = express.Router();
+
+router.post("/register", async (req, res, next) => {
+  const { email, username, password } = req.body;
+  const isAdmin = false;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        error: "UserNameError",
+        message: `User ${username} is already taken.`,
+        name: "UserNameError",
+      });
+    }
+
+    const userEmail = await getUserByEmail(email);
+    if (userEmail) {
+      next({
+        error: "EmailError",
+        message: `Email ${email} is already in use`,
+        name: "EmailError",
+      });
+    }
+
+    const user = await createUser({ email, username, password, isAdmin });
+
+    if (password.length < 8) {
+      next({
+        error: "PasswordError",
+        message: "Password Too Short!",
+        name: "PasswordError",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET
+    );
+
+    res.send({
+      message: "Thanks for signing up",
+      token,
+      user,
+    });
+  } catch ({ error, name, message }) {
+    next({ error, name, message });
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a email and password",
+    });
+  }
+  try {
+    const user = await getUserByEmail(email);
+    if (user && user.password == password) {
+      let token = jwt.sign(
+        {
+          email: user.email,
+          username: user.username,
+          password: user.password,
+          id: user.id,
+        },
+        process.env.JWT_SECRET
+      );
+
+      res.send({ message: "you're logged in!", token, user });
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Email or Password is incorrect",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/me", requireUser, async (req, res, next) => {
+  const user = await getUserByUsername(req.user.username);
+
+  res.send(user);
+});
+
+module.exports = router;
