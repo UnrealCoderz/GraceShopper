0d8af77a99261d2a3b3898b738f5d080f5d166b6
NaseerHines <Hinesnaseer@gmail.com>
2023-07-13 18:03:08 -0500

    simple about page that also shows a featured prod

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import FeaturedProduct from './FeaturedProduct';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Welcome to our e-commerce site! We offer carouis Game accounts, Skins, and other in-game collectibles.</p>
+            <p>Thank you for buying from Unreal Coderz. Happy shopping!</p>
+            <FeaturedProduct />
+        </div>
+    );
+};
+
+export default About;
